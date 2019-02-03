641af2803bf7cc72a314dda0e04ffaeb6aefda07
manish saini <manishdwarkas912@gmail.com>
2019-02-03 20:03:00 +0530

    list

---
+++ list.py
@@ -0,0 +1,15 @@
+#lists and dictonories
+
+print(["mango", "banana", "orange"][0])
+print({"name":"manish", "age":"20"}["name"])
+
+#build in functions are
+#int("5")
+#str(5)
+#bool("True")
+#float(5)
+#len("hello")
+#sorted([5, 4, 2, 1, 3])
+
+
+
