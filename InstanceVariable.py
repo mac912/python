259ad07d4f9e8c1de1dec1b5ad81b252592bc341
manish saini <manishdwarkas912@gmail.com>
2019-02-04 16:02:03 +0530

    importig package form relative path

---
+++ InstanceVariable.py
@@ -0,0 +1,5 @@
+#use of instance variable in class
+from classes.patth import Criket
+obj = Criket(2, 3)
+obj.getball()
+obj.getbat()
---
+++ classes/patth.py
@@ -0,0 +1,8 @@
+class Criket:
+    def __init__(self, ball, bat):
+        self.ball = ball
+        self.bat = bat
+    def getball(self):
+        print("no. of ball:",self.ball)
+    def getbat(self):
+        print("no. of bats:",self.bat)
