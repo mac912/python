9f54ef18cbc8d89b7617558c2328364a3fa1bf10
manish saini <manishdwarkas912@gmail.com>
2019-02-04 15:58:37 +0530

    format for classes in python

---
+++ class.py
@@ -0,0 +1,12 @@
+# format for the class in python
+class Criket:
+    ball=1
+    bats = 2
+    def getball(self):
+        print(self.ball)
+    def getbat(self):
+        print(self.bats)
+
+obj = Criket()
+obj.getball()
+obj.getbat()
